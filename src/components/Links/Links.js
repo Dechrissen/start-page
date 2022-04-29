1fab3250ee863b550720ad262d16f4ed42e1bdaa
Derek Andersen <derekcandersen@gmail.com>
2022-04-28 23:03:03 -0400

    redo start page links for idleon addiction, mostly

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -1,33 +1,31 @@
 import { Wrapper, Container, Heading, Link } from "./Links.style.js";
 
 const Links = () => {
-    return (
-        <Wrapper>
-        
-            <Container>
-                <Heading color="red">mail</Heading>
-                <Link href="https://mail.google.com/mail/u/0/#inbox">gmail</Link>
-                <Link href="https://mail.zoho.com/zm/#mail/folder/inbox">zoho mail</Link>
-                
-            </Container>
+  return (
+    <Wrapper>
+      <Container>
+        <Heading color="blue">social</Heading>
+        <Link href="https://www.youtube.com">youtube</Link>
+        <Link href="https://www.twitch.tv/">twitch</Link>
+        <Link href="https://www.reddit.com/">reddit</Link>
+      </Container>
 
-			<Container>
-                <Heading color="blue">social</Heading>
-                <Link href="https://www.youtube.com">youtube</Link>
-                <Link href="https://www.twitch.tv/">twitch</Link>
-                <Link href="https://www.reddit.com/">reddit</Link>
-            </Container>
+      <Container>
+        <Heading color="red">idleon</Heading>
+        <Link href="https://idleon.info/wiki/Main_Page">wiki</Link>
+        <Link href="https://dechrissen.idleonefficiency.com/">
+          idleonefficiency
+        </Link>
+        <Link href="https://idleoncompanion.com/tasks">companion</Link>
+      </Container>
 
-            <Container>
-                <Heading color="green">etc</Heading>
-                <Link href="https://github.com/">github</Link>
-                <Link href="https://derekandersen.net">personal site</Link>
-                <Link href="https://wiki.archlinux.org/">arch wiki</Link>
-            </Container>
-
-            
-        </Wrapper>
-    );
+      <Container>
+        <Heading color="green">etc</Heading>
+        <Link href="https://github.com/">github</Link>
+        <Link href="https://derekandersen.net">personal site</Link>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Links;
